6bab7e34153f7ac2876cbae33776cddff7a86e01
Joel Delight <thefencingflutist@gmail.com>
2016-08-30 08:07:22 -0700

    commiting to cheange branch, will not push this, Wolfgang is making these same changes

--- js/boot.js
+++ js/boot.js
@@ -7,15 +7,21 @@ Game.Boot = function(game){
 Game.Boot.prototype = {
   init:function(){
     this.input.maxPointers = 1;
-
+    // console.log("init");
     this.stage.disableVisibilityChange = true;
   },
 
   preload:function(){
-    this.load.image('preloaderBar', 'assets/preloader.png');
+    // console.log("preload");
+    this.load.image('preloaderBar', '/assets/preloader.png');
   },
 
   create:function(){
+    console.log(this.state)
+    // console.log("create");
     this.state.start('Preloader');
+    console.log(this.state)
+    debugger
+
   }
 }
